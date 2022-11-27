4905d8ad3019fe56c0197fe9521ecfae7c20c3e8
Hprogram <wjs2096@gmail.com>
2022-11-28 00:10:01 +0900

    index.js에router 적용

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const db = require("../src/db/db.config.js");
+const courseRouter = require("./router/course.router.js");
+const studentRouter = require("./router/student.router.js");
+const teacherRouter = require("./router/teacher.router.js");
 
 // const cors
 const app = express();
@@ -14,6 +17,10 @@ app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use("/course", courseRouter);
+app.use("/student", studentRouter);
+app.use("/teacher", teacherRouter);
 
 app.get("/", function (req, res) {
   res.send("server test");
