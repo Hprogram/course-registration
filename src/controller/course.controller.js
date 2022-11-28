dc8fd982da967ab1f8248a8c7b8a524ba114d0a8
aiden-yh <aiden_yh@soundplatform.co.kr>
2022-11-28 20:28:27 +0900

    course 검색 로직 추가

--- src/controller/course.controller.js
+++ src/controller/course.controller.js
@@ -1,91 +1,161 @@
 const Course = require("../model/course.model.js");
 
 exports.create = (req, res) => {
-  if (!req.body) {
-    res.status(400).send({
-      message: "추가할 데이터가 입력되지 않았습니다.",
-    });
-  }
+	if (!req.body) {
+		res.status(400).send({
+			message: "추가할 데이터가 입력되지 않았습니다.",
+		});
+	}
 
-  const course = new Course({
-    name: req.body.name,
-    description: req.body.description,
-    price: req.body.price,
-    category: req.body.category,
-    // open :req.body.open
-    teacher: req.body.teacher,
-  });
+	const course = new Course({
+		name: req.body.name,
+		description: req.body.description,
+		price: req.body.price,
+		category: req.body.category,
+		// open :req.body.open
+		teacher: req.body.teacher,
+	});
 
-  Course.create(course, (err, data) => {
-    if (err) {
-      res.status(500).send({
-        message: err.message || "error creating Student.",
-      });
-    } else {
-      res.status(201).send(data);
-    }
-  });
+	Course.create(course, (err, data) => {
+		if (err) {
+			res.status(500).send({
+				message: err.message || "error creating Student.",
+			});
+		} else {
+			res.status(201).send(data);
+		}
+	});
 };
 
 exports.update = (req, res) => {
-  if (!req.body.id) {
-    res.status(400).send({
-      message: "id가 입력되지 않았습니다.",
-    });
-  }
+	if (!req.body.id) {
+		res.status(400).send({
+			message: "id가 입력되지 않았습니다.",
+		});
+	}
 
-  Course.updateByID(req.body.id, req.body, (err, data) => {
-    if (err) {
-      if (err.kind === "not_found") {
-        res.status(404).send({
-          message: `Not found Course  with id ${req.params.id}.`,
-        });
-      } else {
-        res.status(500).send({
-          message:
-            err.message || "Error retrieving Course with id " + req.params.id,
-        });
-      }
-    } else res.send(data);
-  });
+	Course.updateByID(req.body.id, req.body, (err, data) => {
+		if (err) {
+			if (err.kind === "not_found") {
+				res.status(404).send({
+					message: `Not found Course  with id ${req.params.id}.`,
+				});
+			} else {
+				res.status(500).send({
+					message:
+						err.message ||
+						"Error retrieving Course with id " + req.params.id,
+				});
+			}
+		} else res.send(data);
+	});
 };
 
 exports.updateOpen = (req, res) => {
-  if (!req.body.id) {
-    res.status(400).send({
-      message: "id가 입력되지 않았습니다.",
-    });
-  }
-  Course.openByID(req.body.id, (err, data) => {
-    if (err) {
-      if (err.kind === "not_found") {
-        res.status(404).send({
-          message: `Not found Course  with id ${req.body.id}.`,
-        });
-      } else {
-        res.status(500).send({
-          message:
-            err.message || "Error retrieving Course with id " + req.body.id,
-        });
-      }
-    } else res.send(data);
-  });
+	if (!req.body.id) {
+		res.status(400).send({
+			message: "id가 입력되지 않았습니다.",
+		});
+	}
+	Course.openByID(req.body.id, (err, data) => {
+		if (err) {
+			if (err.kind === "not_found") {
+				res.status(404).send({
+					message: `Not found Course  with id ${req.body.id}.`,
+				});
+			} else {
+				res.status(500).send({
+					message:
+						err.message ||
+						"Error retrieving Course with id " + req.body.id,
+				});
+			}
+		} else res.send(data);
+	});
 };
 
 exports.delete = (req, res) => {
-  Course.remove(req.params.id, (err, data) => {
-    if (err) {
-      if (err.kind === "not_found") {
-        res.status(404).send({
-          message: `Not found course with id ${req.params.id}.`,
-        });
-      } else {
-        res.status(500).send({
-          message:
-            err.message + " id: " + req.params.id ||
-            "Could not delete course with id " + req.params.id,
-        });
-      }
-    } else res.send({ message: `course was deleted successfully!` });
-  });
+	Course.remove(req.body.id, (err, data) => {
+		if (err) {
+			if (err.kind === "not_found") {
+				res.status(404).send({
+					message: `Not found course with id ${req.body.id}.`,
+				});
+			} else {
+				res.status(500).send({
+					message:
+						err.message + " id: " + req.body.id ||
+						"Could not delete course with id " + req.body.id,
+				});
+			}
+		} else {
+			res.send({ message: `course was deleted successfully!` });
+		}
+	});
+};
+
+exports.findOne = (req, res) => {
+	Course.findOneByID(req.params.id, (err, data) => {
+		if (err) {
+			if (err.kind === "not_found") {
+				res.status(404).send({
+					message: `Not found Course findOne with id ${req.params.id}.`,
+				});
+			} else {
+				res.status(500).send({
+					message:
+						"Error retrieving Course findOne with id " +
+						req.params.id,
+				});
+			}
+		} else {
+			let result = [];
+
+			data.map((el) => {
+				if (result.length === 0) {
+					el.student_count = 1;
+					el.students = [
+						{
+							nickname: el.nickname,
+							created_at: el.cs_created_at,
+						},
+					];
+					el.open = el.open === 0 ? false : true;
+
+					delete el.nickname;
+					delete el.cs_created_at;
+
+					result.push(el);
+				} else {
+					result[0].student_count += 1;
+					result[0].students.push({
+						nickname: el.nickname,
+						created_at: el.cs_created_at,
+					});
+				}
+			});
+
+			res.send(result[0]);
+		}
+	});
+};
+
+exports.find = (req, res) => {
+	Course.findByStudentID(req.params.student_id, (err, data) => {
+		if (err) {
+			if (err.kind === "not_found") {
+				res.status(404).send({
+					message: `Not found Course findOne with id ${req.params.id}.`,
+				});
+			} else {
+				res.status(500).send({
+					message:
+						"Error retrieving Course findOne with id " +
+						req.params.id,
+				});
+			}
+		} else {
+			res.send(data);
+		}
+	});
 };
--- src/router/course.router.js
+++ src/router/course.router.js
@@ -4,15 +4,19 @@ const courseController = require("../controller/course.controller.js");
 const courseRouter = express.Router();
 
 courseRouter.get("/", (req, res, next) => {
-  res.send("course router test");
+	res.send("course router test");
 });
 
 courseRouter.post("/create", courseController.create);
 
 courseRouter.patch("/open/:id", courseController.updateOpen);
 
-courseRouter.delete("/:id", courseController.delete);
+courseRouter.delete("/", courseController.delete);
 
 courseRouter.patch("/", courseController.update);
+
+courseRouter.get("/:id", courseController.findOne);
+
+courseRouter.get("/list/:student_id", courseController.find);
 
 module.exports = courseRouter;
